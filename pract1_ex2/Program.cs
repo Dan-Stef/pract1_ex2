a3d87205bac936c0eef3657cf589d40b83a1ab45
Даниил Стефаненков <daniilstef2391@gmail.com>
2020-06-10 13:42:22 +0300

    Добавьте файлы проекта.

---
+++ pract1_ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract1_ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            Console.WriteLine("Введите двузначное число:");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (!(a  > 9 && a < 100))
+                Console.WriteLine("Число не двузначное");
+            else
+            {
+                b = a / 10;
+                c = a % 10;
+                if (b > c) Console.WriteLine("Первая цифра больше.");
+                else Console.WriteLine("Вторая цифра больше.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
